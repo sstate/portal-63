4404c33c28731265f3d485c6b4c77e87fd6352e5
Richard Torruellas <rtorruellas@gmail.com>
2015-05-18 11:38:23 -0700

    fixing so you dont need to build

--- webpack.config.js
+++ webpack.config.js
@@ -2,56 +2,62 @@ var path = require('path');
 var webpack = require('webpack');
 
 var plugins = [
-    new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  })
 ];
 var entry = ['./examples/app'];
 var loaders = [];
 
 if(process.env.NODE_ENV === 'PRODUCTION'){
-    plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                warnings: false
-            }
-        })
-    );
-    loaders.push(
-        {test: /\.js?$/, exclude: /node_modules/, loaders: ['babel']}
-    );
+  loaders.push(
+    {test: /\.js?$/, exclude: /node_modules/, loaders: ['babel']}
+  );
 }else {
-    plugins.push(
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
-    );
-    entry.push(
-        'webpack-dev-server/client?http://localhost:8180',
-        'webpack/hot/only-dev-server'
-    );
-    loaders.push(
-        {test: /\.js?$/, exclude: /node_modules/, loaders: ['react-hot', 'babel']}
-    );
+  plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  );
+  entry.push(
+    'webpack-dev-server/client?http://localhost:8180',
+    'webpack/hot/only-dev-server'
+  );
+  loaders.push(
+    {test: /\.js?$/, exclude: /node_modules/, loaders: ['react-hot', 'babel']}
+  );
 }
 
 module.exports = {
 
-    entry: entry,
+  entry: entry,
 
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '_bundle.js',
-        publicPath: 'http://localhost:8180/dist'
-    },
+  output: {
+    library: 'Portal',
+    libraryTarget: 'umd',
+    path: path.join(__dirname, 'dist'),
+    filename: 'Portal.js',
+    publicPath: 'http://localhost:8180/dist'
+  },
 
-    module: {
-        loaders: loaders
-    },
+  externals: [
+    {
+      "react": {
+        root: "React",
+        commonjs2: "react",
+        commonjs: "react",
+        amd: "react"
+      }
+    }
+  ],
 
-    node: {
-        Buffer: false
-    },
+  module: {
+    loaders: loaders
+  },
 
-    plugins: plugins
+  node: {
+    Buffer: false
+  },
+
+  plugins: plugins
 
 };
